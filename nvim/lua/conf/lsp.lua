a01e6ce18ccfa960fd2a63df25e2d747dff1be66
Agustin Espana <agustinespania@gmail.com>
2023-01-20 16:57:48 +0100

    updates

--- nvim/lua/conf/lsp.lua
+++ nvim/lua/conf/lsp.lua
@@ -1,6 +1,13 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 lsp.setup()
+lsp.setup_nvim_cmp({
+    preselect = 'none',
+    completion = {
+        completeopt = 'menu,menuone,noinsert,noselect'
+    },
+})
+lsp.setup()
 
 vim.keymap.set("n", 'ca', vim.lsp.buf.code_action, { buffer = bufnr })
 vim.keymap.set("n", 'gd', vim.lsp.buf.definition, { buffer = bufnr })
@@ -26,132 +33,3 @@ vim.diagnostic.config({
     source = 'always'
   },
 })
-
-
--- --  This function gets run when an LSP connects to a particular buffer.
--- local on_attach = function(_, bufnr)
---   -- In this case, we create a function that lets us more easily define mappings specific
---   -- for LSP related items. It sets the mode, buffer and description for us each time.
---   local nmap = function(keys, func, desc)
---     if desc then
---       desc = 'LSP: ' .. desc
---     end
---
---     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
---   end
---
---   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
---
---   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
---   nmap('<leader>r', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
---   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
---   nmap('<leader>t', vim.lsp.buf.type_definition, 'Type [D]efinition')
---   -- nmap('<leader>s', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
---   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
---
---   -- See `:help K` for why this keymap
---   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
---   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
---
---   -- Lesser used LSP functionality
---   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
---   nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
---   nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
---   nmap('<leader>wl', function()
---     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
---   end, '[W]orkspace [L]ist Folders')
---
---   -- Create a command `:Format` local to the LSP buffer
---   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
---     vim.lsp.buf.format()
---   end, { desc = 'Format current buffer with LSP' })
--- end
---
--- -- Enable the following language servers
--- local servers = {
---   sumneko_lua = {
---     Lua = {
---       workspace = { checkThirdParty = false },
---       telemetry = { enable = false },
---     },
---   },
--- }
---
--- require('nvim-treesitter.configs').setup {
---   ensure_installed = { 'lua', 'python', 'java', 'typescript', 'tsx', 'css', 'html', 'scss', 'javascript', 'json', 'help' },
--- }
---
--- require('mason').setup()
--- local mason_lspconfig = require 'mason-lspconfig'
--- mason_lspconfig.setup {
---   ensure_installed = vim.tbl_keys(servers),
--- }
--- mason_lspconfig.setup_handlers {
---   function(server_name)
---     require('lspconfig')[server_name].setup {
---       capabilities = capabilities,
---       on_attach = on_attach,
---       settings = servers[server_name],
---     }
---   end,
--- }
---
--- require('fidget').setup()
---
--- vim.diagnostic.config({
---   virtual_text = false,
---   severity_sort = true,
---   update_in_insert = false,
---   float = {
---     source = 'always'
---   },
--- })
--- vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
--- -- vim.keymap.set('n', 'gd', ":lua vim.diagnostic.setloclist()<CR>")
--- vim.keymap.set('n', 'ca', vim.lsp.buf.code_action)
---
--- -- nvim-cmp setup
--- local cmp = require 'cmp'
--- local luasnip = require 'luasnip'
---
--- cmp.setup {
---   snippet = {
---     expand = function(args)
---       luasnip.lsp_expand(args.body)
---     end,
---   },
---   mapping = cmp.mapping.preset.insert {
---     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
---     ['<C-f>'] = cmp.mapping.scroll_docs(4),
---     ['<C-Space>'] = cmp.mapping.complete(),
---     ['<CR>'] = cmp.mapping.confirm {
---       behavior = cmp.ConfirmBehavior.Replace,
---       select = true,
---     },
---     ['<Tab>'] = cmp.mapping(function(fallback)
---       if cmp.visible() then
---         cmp.select_next_item()
---       elseif luasnip.expand_or_jumpable() then
---         luasnip.expand_or_jump()
---       else
---         fallback()
---       end
---     end, { 'i', 's' }),
---     ['<S-Tab>'] = cmp.mapping(function(fallback)
---       if cmp.visible() then
---         cmp.select_prev_item()
---       elseif luasnip.jumpable(-1) then
---         luasnip.jump(-1)
---       else
---         fallback()
---       end
---     end, { 'i', 's' }),
---   },
---   sources = {
---     {name = 'path'},
---     {name = 'nvim_lsp', keyword_length = 3},
---     {name = 'buffer', keyword_length = 3},
---     -- {name = 'luasnip', keyword_length = 2},
---   },
--- }
---
