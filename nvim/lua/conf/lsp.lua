4842f420e90c7e6edb8d99e5b1b810acd8d2af91
Agustin Espana <agustinespania@gmail.com>
2023-06-30 14:19:55 +0200

    added error always on

--- nvim/lua/conf/lsp.lua
+++ nvim/lua/conf/lsp.lua
@@ -66,7 +66,7 @@ require('nvim-treesitter.configs').setup {
 }
 
 vim.diagnostic.config({
-  virtual_text = false,
+  virtual_text = true,
   severity_sort = true,
   update_in_insert = false,
   float = {
