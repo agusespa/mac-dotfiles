4540656ba5422005c5192e3b1a0a94a44a125f74
Agustin Espana <agustinespania@gmail.com>
2023-07-05 13:37:21 +0200

    fix

--- nvim/lua/conf/mappings.lua
+++ nvim/lua/conf/mappings.lua
@@ -8,28 +8,32 @@
 
 vim.g.mapleader = '\''
 
+local default_opts = {noremap = true, silent = true}
+
 -- Nvim features
-vim.keymap.set("i", "kj", "<Esc>", { noremap = true })
-vim.keymap.set("n", "<leader>q", ":bd<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>s", ":update<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>e", ":Explore<CR>", { noremap = true, silent = true })
+vim.keymap.set("i", "kj", "<Esc>", default_opts)
+vim.keymap.set("n", "<leader>q", ":bd<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>s", ":update<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>e", ":Explore<CR>", default_opts)
 
 -- Code formatting
-vim.api.nvim_set_keymap('n', 'cs', ':lua FormatCode()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'cs', ':lua FormatCode()<CR>', { noremap = true, silent = false })
 function FormatCode()
-  local filetype = vim.bo.filetype
-  if filetype:match('html|ts|js|css|tsx|jsx|scss') then
+  local ft = vim.bo.filetype
+  if ft == "typescript" or ft == "javascript" or ft == "tsx" or ft == "html" or ft == "css" or ft == "scss" or ft == "jsx" or ft == "json" then
     vim.cmd(':Prettier')
-  else
+  elseif ft == "java" or ft == "c" then
     vim.cmd(':ClangFormat')
+  elseif ft == "python" then
+    print("Unsupported format: " .. ft)
+  else
+    print("Unsupported format: " .. ft)
   end
 end
--- vim.keymap.set("n", "cs", ":ClangFormat<CR>", { noremap = true, silent = true })
--- vim.keymap.set("n", "cp", ":Prettier<CR>", { noremap = true, silent = true })
 
 -- LSPs
 vim.keymap.set("n", '<leader>r', require('telescope.builtin').lsp_references)
-vim.keymap.set('n', '<leader>d', ':Telescope diagnostics<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>d', ':Telescope diagnostics<CR>', default_opts)
 vim.keymap.set("n", 'gd', vim.lsp.buf.definition)
 vim.keymap.set("n", 'gt', vim.lsp.buf.type_definition)
 vim.keymap.set("n", 'sd', vim.lsp.buf.hover)
@@ -39,13 +43,12 @@ vim.keymap.set("n", 'cr', vim.lsp.buf.rename)
 
 -- Telescope
 local builtin = require('telescope.builtin')
-local default_opts = {noremap = true, silent = true}
-vim.keymap.set('n', '<leader>f', ":Telescope find_files hidden=true<CR>", { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>g', builtin.live_grep, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>b', builtin.buffers, { noremap = true, silent = true })
-vim.keymap.set("n", '<leader>n', ":Telescope file_browser path=%:p:h hidden=true<CR>", { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>f', ":Telescope find_files hidden=true<CR>", default_opts)
+vim.keymap.set('n', '<leader>g', builtin.live_grep, default_opts)
+vim.keymap.set('n', '<leader>b', builtin.buffers, default_opts)
+vim.keymap.set("n", '<leader>n', ":Telescope file_browser path=%:p:h hidden=true<CR>", default_opts)
 vim.api.nvim_set_keymap('v', '<leader>g', 'y<ESC>:Telescope live_grep default_text=<c-r>0<CR>', default_opts)
-vim.keymap.set('n', '<leader>c', builtin.git_status, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>c', builtin.git_status, default_opts)
 
 -- Harpoon
 vim.keymap.set('n', '<leader>h', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true, silent = true })
