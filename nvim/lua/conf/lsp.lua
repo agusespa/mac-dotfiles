a74b87a8a590beeaf5ffb99a6d1e3fd59ce5d1e2
Agustin Espana <agustinespania@gmail.com>
2023-08-16 09:29:44 +0200

    fixed angular lsp

--- nvim/lua/conf/lsp.lua
+++ nvim/lua/conf/lsp.lua
@@ -31,39 +31,39 @@ lspconfig.golangci_lint_ls.setup({})
 lspconfig.bufls.setup{}
 lspconfig.jdtls.setup{}
 
--- Angular setup
-local uv = vim.loop
-local util = require "lspconfig.util"
-local function get_node_modules(root_dir)
-  local root_node = root_dir .. "/node_modules"
-  local stats = uv.fs_stat(root_node)
-  if stats == nil then
-    return ''
-  else
-    return root_node
-  end
-end
-local default_node_modules = get_node_modules(vim.fn.getcwd())
-local ngls_cmd = {
-  "ngserver",
-  "--stdio",
-  "--tsProbeLocations",
-  default_node_modules,
-  "--ngProbeLocations",
-  default_node_modules,
-  "--includeCompletionsWithSnippetText",
-  "--includeAutomaticOptionalChainCompletions",
-}
-lspconfig.angularls.setup {
-  filetypes = { "typescript", "html" },
-  cmd = ngls_cmd,
-  on_attach = on_attach,
-  capabilities = capabilities,
-  root_dir = util.root_pattern("angular.json"),
-  on_new_config = function(new_config)
-    new_config.cmd = ngls_cmd
-  end
-}
+lspconfig.angularls.setup{}
+-- OLD Angular setup
+-- local uv = vim.loop
+-- local util = require "lspconfig.util"
+-- local function get_node_modules(root_dir)
+--   local root_node = root_dir .. "/node_modules"
+--   local stats = uv.fs_stat(root_node)
+--   if stats == nil then
+--     return ''
+--   else
+--     return root_node
+--   end
+-- end
+-- local default_node_modules = get_node_modules(vim.fn.getcwd())
+-- local ngls_cmd = {
+--   "ngserver",
+--   "--stdio",
+--   "--tsProbeLocations",
+--   default_node_modules,
+--   "--ngProbeLocations",
+--   default_node_modules,
+--   "--includeCompletionsWithSnippetText",
+--   "--includeAutomaticOptionalChainCompletions",
+-- }
+-- lspconfig.angularls.setup {
+--   filetypes = { "typescript", "html" },
+--   cmd = ngls_cmd,
+--   capabilities = capabilities,
+--   root_dir = util.root_pattern("angular.json"),
+--   on_new_config = function(new_config)
+--     new_config.cmd = ngls_cmd
+--   end
+-- }
 -- END of Angular setup
 
 
