70d6a99fc3bdf907c60033bb69ca3afadc6d4212
Agustin Espana <agustinespania@gmail.com>
2023-12-13 13:21:06 +0100

    updated mapping

--- nvim/lua/conf/lsp.lua
+++ nvim/lua/conf/lsp.lua
@@ -88,3 +88,13 @@ cmp.setup({
     { name = 'buffer' },
   })
 })
+
+require('tabnine').setup({
+  disable_auto_comment=true,
+  accept_keymap="<CR>",
+  dismiss_keymap = "<Left>",
+  debounce_ms = 800,
+  suggestion_color = {gui = "#808080", cterm = 244},
+  exclude_filetypes = {"TelescopePrompt", "NvimTree"},
+  log_file_path = nil, -- absolute path to Tabnine log file
+})
--- nvim/lua/conf/plugins.lua
+++ nvim/lua/conf/plugins.lua
@@ -16,6 +16,7 @@ require('packer').startup(function(use)
         {'hrsh7th/cmp-path'},
         {'hrsh7th/cmp-nvim-lsp'},
         {'hrsh7th/cmp-cmdline'},
+        { 'codota/tabnine-nvim', run = "./dl_binaries.sh" },
         -- Snippets
         {'L3MON4D3/LuaSnip'},
         {'saadparwaiz1/cmp_luasnip'},
