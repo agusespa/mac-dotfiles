10b6531b4ca1923eb73152493c10884da0ba036d
Agustin Espana <agustinespania@gmail.com>
2024-04-09 13:01:19 +0200

    feat: disabled tabinine

--- nvim/lua/conf/lsp.lua
+++ nvim/lua/conf/lsp.lua
@@ -109,12 +109,12 @@ cmp.setup({
   })
 })
 
-require('tabnine').setup({
-  disable_auto_comment = true,
-  accept_keymap = "<CR>",
-  dismiss_keymap = "<Left>",
-  debounce_ms = 800,
-  suggestion_color = { gui = "#808080", cterm = 244 },
-  exclude_filetypes = { "TelescopePrompt", "NvimTree" },
-  log_file_path = nil, -- absolute path to Tabnine log file
-})
+-- require('tabnine').setup({
+--   disable_auto_comment = true,
+--   accept_keymap = "<CR>",
+--   dismiss_keymap = "<Left>",
+--   debounce_ms = 800,
+--   suggestion_color = { gui = "#808080", cterm = 244 },
+--   exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+--   log_file_path = nil, -- absolute path to Tabnine log file
+-- })
