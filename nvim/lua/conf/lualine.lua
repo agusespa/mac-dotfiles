0dbaed6df2e366e75e660851711a8e59e1e9d669
Agustin Espana <agustinespania@gmail.com>
2024-07-10 10:19:20 +0200

    updated lualine options

--- nvim/lua/conf/lualine.lua
+++ nvim/lua/conf/lualine.lua
@@ -18,54 +18,61 @@ require('lualine').setup {
     }
   },
   sections = {
-    lualine_a = {'mode'},
+    lualine_a = { 'mode' },
     lualine_b = {
-        'branch',
-        'diff',
+      'branch',
+      'diff',
     },
     lualine_c = {
-        {
-          'filename',
-          file_status = true,      -- Displays file status (readonly status, modified status)
-          newfile_status = false,   -- Display new file status (new file means no write after created)
-          path = 1,                -- 0: Just the filename
-                                   -- 1: Relative path
-                                   -- 2: Absolute path
-                                   -- 3: Absolute path, with tilde as the home directory
-          shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
-          symbols = {
-            modified = '[+]',      -- Text to show when the file is modified.
-            readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
-            unnamed = '[No Name]', -- Text to show for unnamed buffers.
-            newfile = '[New]',     -- Text to show for new created file before first writting
-          },
+      {
+        'filename',
+        file_status = true,     -- Displays file status (readonly status, modified status)
+        newfile_status = true, -- Display new file status (new file means no write after created)
+        path = 1,               -- 0: Just the filename
+        -- 1: Relative path
+        -- 2: Absolute path
+        -- 3: Absolute path, with tilde as the home directory
+        shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+        symbols = {
+          modified = '[+]',      -- Text to show when the file is modified.
+          readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+          unnamed = '[No Name]', -- Text to show for unnamed buffers.
+          newfile = '[New]',     -- Text to show for new created file before first writting
+        },
         color = { bg = '#1e1e2e' },
-        }
+      },
+      {
+        'diagnostics',
+        sections = { 'error', 'warn', 'info', 'hint' },
+        -- diagnostics_color = {
+        --   error = { bg = '#1d2021', fg = '#cc241d' }, -- Changes diagnostics' error color.
+        --   warn  = { bg = '#1d2021', fg = '#fabd2f' },  -- Changes diagnostics' warn color.
+        --   info  = { bg = '#1d2021', fg = '#fbf1c7' },  -- Changes diagnostics' info color.
+        --   hint  = { bg = '#1d2021', fg = '#8ec07c' },  -- Changes diagnostics' hint color.
+        -- },
+        symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
+        colored = true,           -- Displays diagnostics status in color if set to true.
+        update_in_insert = false, -- Update diagnostics in insert mode.
+        always_visible = false,   -- Show diagnostics even if there are none.
+        color = { bg = '#1e1e2e' },
+      }
     },
     lualine_x = {
-        {
-            'encoding',
-            color = { bg = '#1e1e2e' },
-        }
-    },
-    lualine_y = {'filetype'},
-    lualine_z = {
-        'progress', 'location',
-        {
-          'diagnostics',
-          sections = { 'error', 'warn', 'info', 'hint' },
-          -- diagnostics_color = {
-          --   error = { bg = '#1d2021', fg = '#cc241d' }, -- Changes diagnostics' error color.
-          --   warn  = { bg = '#1d2021', fg = '#fabd2f' },  -- Changes diagnostics' warn color.
-          --   info  = { bg = '#1d2021', fg = '#fbf1c7' },  -- Changes diagnostics' info color.
-          --   hint  = { bg = '#1d2021', fg = '#8ec07c' },  -- Changes diagnostics' hint color.
-          -- },
-          symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
-          colored = true,           -- Displays diagnostics status in color if set to true.
-          update_in_insert = false, -- Update diagnostics in insert mode.
-          always_visible = false,   -- Show diagnostics even if there are none.
-        }
+      {
+        'filetype',
+        color = { bg = '#1e1e2e' },
       }
+    },
+    lualine_y = {
+      {
+        'encoding',
+        color = { bg = '#1e1e2e' },
+      },
+    },
+    lualine_z = {
+      'progress',
+      'location',
+    }
   },
   inactive_sections = {
     lualine_a = {},
