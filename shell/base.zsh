e14779e2496c7372f5df364739246f5d6171216a
Agustin Espana <agustinespania@gmail.com>
2025-04-15 15:10:19 +0200

    feat: updated PATH

--- shell/base.zsh
+++ shell/base.zsh
@@ -4,16 +4,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-export PATH="$PATH:/Applications/Postgres.app/Contents/Versions/14/bin"
 export PATH="$PATH:/opt/homebrew/bin"
 export PATH="$PATH:$HOME/.jenv/bin"
 export PATH="$PATH:$HOME/.cargo/bin"
-export PATH="$PATH:$HOME/Library/Android/sdk/platform-tools"
-export PATH="$PATH:$HOME/Library/Android/sdk/tools"
 export PATH="$PATH:$HOME/Code/go-lib/bin"
 export PATH="$PATH:$HOME/Code/cli-tools/mux"
-export PATH="$PATH:$ANDROID_HOME/emulator"
-export PATH="$PATH:$ANDROID_HOME/platform-tools"
 
 export C_INCLUDE_PATH="/opt/homebrew/include/glib-2.0"
 export GOPATH="$HOME/Code/go-lib"
