cc8d2889f1d8003cf633daed8346d80cf7a0a292
Agustin Espana <agustinespania@gmail.com>
2025-04-16 15:48:44 +0200

    feat: improved logic

--- shell/prompt.zsh
+++ shell/prompt.zsh
@@ -3,15 +3,15 @@ local neutral_color="%F{145}"
 local gitdiff_color="%F{221}"
 local reset_color="%f"
 
-function parse_git_branch() {
-  if git rev-parse --abbrev-ref HEAD >/dev/null 2>&1; then
-    local branch=$(git rev-parse --abbrev-ref HEAD)
-    local dirty=$(git status --porcelain 2>/dev/null)
-    if [[ -n $dirty ]]; then
-      echo "${neutral_color}[${branch}${gitdiff_color}*${neutral_color}] "
-    else
-      echo "${neutral_color}[${branch}${neutral_color}] "
-    fi
+parse_git_branch() {
+  local branch dirty
+  branch=$(git symbolic-ref --short HEAD 2>/dev/null) || return
+
+  dirty=$(git status --porcelain 2>/dev/null)
+  if [[ -n $dirty ]]; then
+    echo "${neutral_color}[${branch}${gitdiff_color}*${neutral_color}] "
+  else
+    echo "${neutral_color}[${branch}] "
   fi
 }
 
